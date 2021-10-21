7f1e9da2cef7cd869aca9232c38d62cd2c6963cb
cmccurd <cam.alex.mccurdy@gmail.com>
2021-10-21 15:24:46 -0400

    Heroku commit

--- server/index.js
+++ server/index.js
@@ -56,7 +56,10 @@ app.get('/repos', function (req, res) {
 
 });
 
-let port = 1128;
+let port = process.env.PORT//1128;
+if (port == null || port == "") {
+  port = 1128;
+}
 
 app.listen(port, function () {
   console.log(`listening on port ${port}`);
